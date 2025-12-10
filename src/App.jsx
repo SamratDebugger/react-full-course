c17825833180ee6e0ff8e49640fb486a8e6162b9
Samrat <md721023@gmail.com>
2025-12-11 03:26:40 +0600

    rendering correction

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 export default function App() {
-  const status = false;
+  const status = true;
   switch (status) {
     case true:
       return <button>logout</button>;
     case false:
       return <button>login</button>;
+    default:
       return null;
   }
 }
